28777280d540fb2441dbb137ff83f9deed38a653
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2025-06-05 11:32:48 -0600

    [CI] Entorno Configurado

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PRUEBA_TECNICA_IMOVS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PRUEBA_TECNICA_IMOVS.Models.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(PRUEBA_TECNICA_IMOVS.Models.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
