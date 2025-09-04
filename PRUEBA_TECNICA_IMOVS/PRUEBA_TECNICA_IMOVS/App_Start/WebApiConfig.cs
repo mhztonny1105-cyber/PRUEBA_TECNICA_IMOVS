4a122475dbaf51bcc15a1e6de0617b5085102edd
ClaraGarciaAcademicProfile <clara.garcia@ibt.unam.mx>
2025-09-04 15:25:38 -0600

    ya está toda la API REST lista (CRUD de Productos, Tickets y Pagos usando EF). Falta testear más.

--- PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/App_Start/WebApiConfig.cs
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/App_Start/WebApiConfig.cs
@@ -11,14 +11,19 @@ namespace PRUEBA_TECNICA_IMOVS
         {
             // Configuración y servicios de Web API
 
-            // Rutas de Web API
+            // Configuración de atributos de ruta de Web API
             config.MapHttpAttributeRoutes();
 
+            // Rutas de Web API
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Configurar JSON como formato predeterminado
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(
+                new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
