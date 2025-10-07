99ab5f41f9205b70ef62169c059c79cde0dce153
Tatiana Moreno <96274777+TatoMC@users.noreply.github.com>
2025-10-06 20:51:37 -0600

    Add files via upload
    Archivos controladores

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Controllers/TicketCompraController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRUEBA_TECNICA_IMOVS.Controllers
+{
+    public class TicketCompraController : Controller
+    {
+        // GET: TicketCompra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TicketCompra/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: TicketCompra/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketCompra/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketCompra/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketCompra/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TicketCompra/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TicketCompra/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
