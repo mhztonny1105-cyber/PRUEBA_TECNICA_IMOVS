e7fc8720b7aa23de1dae8399ccec277b74deec8a
Tatiana Moreno <96274777+TatoMC@users.noreply.github.com>
2025-10-06 20:55:17 -0600

    Add files via upload

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PRUEBA_TECNICA_IMOVS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
