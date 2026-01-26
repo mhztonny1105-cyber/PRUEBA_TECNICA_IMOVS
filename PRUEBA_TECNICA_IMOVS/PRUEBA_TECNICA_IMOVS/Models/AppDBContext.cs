30fc217ef9d4fd8b0e9913369426034acd5a9a3e
Fabian Ortiz JM <garfiun18@gmail.com>
2026-01-26 13:57:05 -0600

    Create AppDBContext.cs
    Creacion de DBContext para manejo de relaciones y conexion a base de datos

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/AppDBContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketDetalle> TicketDetalles { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ticket>()
+                .HasMany(t => t.Detalles)
+                .WithRequired(d => d.Ticket)
+                .HasForeignKey(d => d.TicketId)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Ticket>()
+                .HasMany(t => t.Pagos)
+                .WithRequired(p => p.Ticket)
+                .HasForeignKey(p => p.TicketId)
+                .WillCascadeOnDelete(true);
+        }
+    }
+}
