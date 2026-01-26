a9113f5a0cb6069e67f908a9a9050952cc8c87bc
Fabian Ortiz JM <garfiun18@gmail.com>
2026-01-26 13:27:16 -0600

    Create Producto.cs
    Creacion del modelo Producto y sus campos con reglas de validacion

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    [Table("Productos")]
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [StringLength(100)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PrecioUnitario { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
