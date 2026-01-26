e68d017173d755780f422fd2c9e051363bf88def
Fabian Ortiz JM <garfiun18@gmail.com>
2026-01-26 13:43:12 -0600

    Create Pago.cs
    Creacion del modelo Pago y sus validaciones de datos

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Pago.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    [Table("Pagos")]
+    public class Pago
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PagoId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Folio { get; set; }
+
+        [Required]
+        public int TicketId { get; set; }
+
+        [Required]
+        public int NumeroPago { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+        public decimal Monto { get; set; }
+
+        [Required]
+        public DateTime FechaPago { get; set; }
+
+        [StringLength(500)]
+        public string Observaciones { get; set; }
+
+        // Relación
+        [ForeignKey("TicketId")]
+        public virtual Ticket Ticket { get; set; }
+
+        public Pago()
+        {
+            FechaPago = DateTime.Now;
+        }
+    }
+}
