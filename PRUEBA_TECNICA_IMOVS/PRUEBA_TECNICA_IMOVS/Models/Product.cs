08b88b3d34264f6a4292d92b8b8a0d1e23ffd10c
mrMcAko <alechacin08@gmail.com>
2026-01-26 19:32:16 -0600

    feat: Se agrego el Sku y descripcion del producto para volverlo mas completo

--- PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Product.cs
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Product.cs
@@ -7,10 +7,17 @@ namespace PRUEBA_TECNICA_IMOVS.Models
     {
         [Key]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Sku { get; set; }
 
         [Required]
         [MaxLength(100)]
         public string Name { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
 
         [Required]
         public decimal Price { get; set; }
