f5a57a1d3f7a85325e79defd366d047760a12066
mrMcAko <alechacin08@gmail.com>
2026-01-26 19:57:10 -0600

    feat: Se agrego al modelado de ticket el DateTime para saber en que momento fue pagado este

--- PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Ticket.cs
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Ticket.cs
@@ -12,12 +12,15 @@ namespace PRUEBA_TECNICA_IMOVS.Models
         [MaxLength(50)]
         public string Folio { get; set; }
 
+        public DateTime? PaidAt { get; set; }
+
         [Required]
         public decimal TotalAmount { get; set; }
 
         [Required]
         public decimal PendingAmount { get; set; }
 
+        [Required]
         public TicketStatus Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
