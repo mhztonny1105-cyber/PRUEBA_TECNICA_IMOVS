fe510bba118b7943febe83c7db30e80b5c95aba9
mrMcAko <alechacin08@gmail.com>
2026-01-26 18:55:03 -0600

    feat: Se creo el modelado de los Tickets junto con sus validaciones, además se creó e hizo uso del status de los tickets, donde se visualizan 3 posibles valores (Pending, InProgress, Paid).

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Ticket.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public class Ticket 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Folio { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public decimal PendingAmount { get; set; }
+
+        public TicketStatus Status { get; set; }
+    }
+}
---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/TicketStatus.cs
@@ -0,0 +1,9 @@
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public enum TicketStatus
+    {
+        Pending = 1,
+        InProgress = 2,
+        Paid = 3
+    }
+}
