1e62b11b28ee2247cf7720ba63d70d5590cdfd28
mrMcAko <alechacin08@gmail.com>
2026-01-26 18:47:36 -0600

    feat: Se crea el modelo productos con sus respectivas validaciones.

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Products.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public bool IsActive { get; set; } = true;
+    }
+}
