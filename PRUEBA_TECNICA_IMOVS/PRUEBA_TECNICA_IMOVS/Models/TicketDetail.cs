d19dec38957761c1ce46082450b1eb1bef8682f4
mrMcAko <alechacin08@gmail.com>
2026-01-26 19:02:38 -0600

    feat: Se crea el modelo TicketDetail el cual hace uso de los Id como fk del modelo Ticket y del modelo Product. Además se agregaron los demas atributos del modelo asi como sus validaciones.

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/TicketDetail.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public class TicketDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int TicketId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+
+        [ForeignKey ("TicketId")]
+        public virtual Ticket Ticket { get; set; }
+
+        [ForeignKey ("ProductId")]
+        public virtual Product Product { get; set; }
+
+    }
+}
