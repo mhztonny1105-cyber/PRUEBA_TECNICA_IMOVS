05ca12ff717d3d8ba582965b2ecf4d010770f153
mrMcAko <alechacin08@gmail.com>
2026-01-26 19:09:43 -0600

    feat: Se crea el Modelado del payment, incluyendo el identificador de los tickets. También agregamos los atributos para el modelo completo, incluyendo sus validaciones.

---
+++ PRUEBA_TECNICA_IMOVS/PRUEBA_TECNICA_IMOVS/Models/Payment.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRUEBA_TECNICA_IMOVS.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int TicketId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Folio { get; set; }
+
+        [Required]
+        public int PaymentNumber { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        public DateTime PaymantDate { get; set; } = DateTime.Now;
+
+        [ForeignKey ("TicketId")]
+        public virtual Ticket Ticket { get; set; }
+    }
+}
